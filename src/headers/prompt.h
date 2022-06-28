4284fabacdcb8824344e863795491bfc9c636c50
potato <lanveble08@gmail.com>
2022-06-28 09:25:03 +0200

    The prompt is now configurable as PS1

--- src/headers/prompt.h
+++ src/headers/prompt.h
@@ -2,13 +2,53 @@
 #define PROMPT_HEADER
 
 const char *prompt(const char *homepath);
+void str_replace(char *stack, const char *needle, const char *replacement);
 
-char shell_prompt[512];
+char shell_prompt[1024];
+
+void str_replace(char *stack, const char *needle, const char *replacement)
+{
+    char buffer[1024] = {0};
+    char *insert_point = &buffer[0];
+    const char *tmp = stack;
+    size_t needle_len = strlen(needle);
+    size_t repl_len = strlen(replacement);
+
+    while (1) {
+        const char *p = strstr(tmp, needle);
+
+        // Walked past last occurrence of needle; copy remaining part
+        if (p == NULL) {
+            strcpy(insert_point, tmp);
+            break;
+        }
+
+        // Copy part before needle
+        memcpy(insert_point, tmp, p - tmp);
+        insert_point += p - tmp;
+
+        // Copy replacement string
+        memcpy(insert_point, replacement, repl_len);
+        insert_point += repl_len;
+
+        tmp = p + needle_len;
+    }
+
+    // Write the finished string back to stack
+    strcpy(stack, buffer);
+}
 
 const char *prompt(const char *homepath){
     // Create a prompt for the user
     char cwd[PATHLEN];
     char *cwd_check;
+    char *PS1 = getenv("PS1");
+    if (!PS1){
+        fprintf(stderr, "kosmos: prompt not found, using default one\n");
+        strcpy(shell_prompt, "$ ");
+    } else {
+        strcpy(shell_prompt, PS1);
+    }
 
     cwd_check = getcwd(cwd, PATHLEN);
     if (!cwd_check){
@@ -28,11 +68,28 @@ const char *prompt(const char *homepath){
     }
     free(path);
     char *host = get_host();
-    // Combine everything into a string and return it
-    snprintf(shell_prompt, sizeof(shell_prompt), "[%s%s%s@%s%s%s]:%s%s%s$ ",
-            YELLOWBOLD, whoami(), RESET,
-            BLUE, host, RESET,
-            MAGENTA, cwd, RESET);
+
+    // User
+    str_replace(shell_prompt, "\\u", whoami());
+    // Host
+    str_replace(shell_prompt, "\\h", host);
+    // cwd
+    str_replace(shell_prompt, "\\s", cwd);
+
+    char *color_arr[] = {RED, REDBOLD, GREEN, GREENBOLD,
+                        YELLOW, YELLOWBOLD, BLUE, BLUEBOLD,
+                        MAGENTA, MAGENTABOLD, CYAN, CYANBOLD,
+                        RESET};
+    char *color_symbols[] = {"\\r\\", "\\rb\\", "\\g\\", "\\gb\\",
+                        "\\y\\", "\\yb\\", "\\b\\", "\\bb\\",
+                        "\\m\\", "\\mb\\", "\\c\\", "\\cb\\",
+                        "\\reset\\"}; 
+    
+    // Asign a symbol for every color
+    for (int i = 0; i < SIZE(color_arr); i++){
+        str_replace(shell_prompt, color_symbols[i], color_arr[i]);
+    }
+
     free(host);
     return shell_prompt;
 }
